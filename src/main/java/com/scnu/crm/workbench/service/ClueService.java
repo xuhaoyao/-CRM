f3a071aad8059b45f56e697a64d143bd375f79f0
xhy <623834276@qq.com>
2021-05-16 01:04:03 +0800

    业务层(线索)

--- src/main/java/com/scnu/crm/workbench/service/ClueService.java
+++ src/main/java/com/scnu/crm/workbench/service/ClueService.java
@@ -2,6 +2,7 @@ package com.scnu.crm.workbench.service;
 
 import com.scnu.crm.vo.PaginationVo;
 import com.scnu.crm.workbench.domain.Clue;
+import com.scnu.crm.workbench.domain.Tran;
 
 import java.util.Map;
 
@@ -15,4 +16,6 @@ public interface ClueService {
     boolean disssociation(String id);
 
     boolean association(Map<String, Object> map);
+
+    boolean convert(String clueId, Tran t, String createBy, String createTime);
 }
